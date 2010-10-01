69f67d2e863c392fa3338f14af142f583723ba5e
Brandon Nortum <ban69@sage.southhills.edu>
2010-10-01 16:11:12 -0400

    Second Commit

--- XmlReader/frmWeatherReader.Designer.vb
+++ XmlReader/frmWeatherReader.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frmWeatherReader
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,29 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnClickMe = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnClickMe
+        '
+        Me.btnClickMe.Location = New System.Drawing.Point(115, 53)
+        Me.btnClickMe.Name = "btnClickMe"
+        Me.btnClickMe.Size = New System.Drawing.Size(75, 23)
+        Me.btnClickMe.TabIndex = 0
+        Me.btnClickMe.Text = "Click Me"
+        Me.btnClickMe.UseVisualStyleBackColor = True
+        '
+        'frmWeatherReader
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(323, 136)
+        Me.Controls.Add(Me.btnClickMe)
+        Me.Name = "frmWeatherReader"
+        Me.Text = "I'll get the Degrees of fun it is outside"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents btnClickMe As System.Windows.Forms.Button
 
 End Class
---
+++ XmlReader/frmWeatherReader.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmWeatherReader
+
+    Private Sub btnClickMe_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClickMe.Click
+
+    End Sub
+End Class
